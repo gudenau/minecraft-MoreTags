a958d7555ec520bb9b7e615742dd0b64a9f12612
gudenau <gudenau@users.noreply.github.com>
2021-08-25 18:17:48 -0500

    Loom bug work around

--- src/main/java/net/gudenau/minecraft/moretags/mixins/farmland/RabbitEntity$EatCarrotCropGoalMixin.java
+++ src/main/java/net/gudenau/minecraft/moretags/mixins/farmland/RabbitEntity$EatCarrotCropGoalMixin.java
@@ -9,12 +9,14 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(targets = "net/minecraft/entity/passive/RabbitEntity$EatCarrotCropGoal")
 public abstract class RabbitEntity$EatCarrotCropGoalMixin{
+    // Loom is dumb at times, this should fix an issue with it.
     @Redirect(
-        method = "isTargetPos",
+        method = {"isTargetPos", "method_6296"},
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"
-        )
+        ),
+        remap = false
     )
     private boolean isTargetFarmland(BlockState blockState, Block block){
         return blockState.isIn(MoreTags.FARMLAND);
