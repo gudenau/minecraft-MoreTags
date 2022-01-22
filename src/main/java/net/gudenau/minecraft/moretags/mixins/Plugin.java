99323fcc45b3f82be5dccfd2463a0a9ee6bcec1d
gudenau <gudenau@users.noreply.github.com>
2022-01-22 15:52:02 -0600

    All the Blocks You Want CompatibilityLayer

--- src/main/java/net/gudenau/minecraft/moretags/mixins/Plugin.java
+++ src/main/java/net/gudenau/minecraft/moretags/mixins/Plugin.java
@@ -1,26 +1,122 @@
 package net.gudenau.minecraft.moretags.mixins;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 import java.util.Set;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.SemanticVersion;
 import net.fabricmc.loader.api.VersionParsingException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
 public final class Plugin implements IMixinConfigPlugin{
-    private static final boolean DEV;
-    static{
-        boolean dev;
-        try{
-            dev = Files.isDirectory(Path.of(Plugin.class.getProtectionDomain().getCodeSource().getLocation().toURI()));
-        }catch(Throwable t){
-            dev = false;
+    private static final boolean DEV = FabricLoader.getInstance().isDevelopmentEnvironment();
+    
+    private static final Logger LOGGER = LogManager.getLogger("MoreTags");
+    
+    private enum VersionCheckType{
+        ALWAYS((suppliedVersion, expectedVersion)->true),
+        OLDER_THAN((suppliedVersion, expectedVersion)->suppliedVersion.compareTo(expectedVersion) > 0),
+        NEWER_THAN((suppliedVersion, expectedVersion)->suppliedVersion.compareTo(expectedVersion) < 0),
+        EQUAL_TO((suppliedVersion, expectedVersion)->suppliedVersion.equals(expectedVersion))
+        ;
+    
+        private final BiPredicate<SemanticVersion, SemanticVersion> predicate;
+    
+        VersionCheckType(BiPredicate<SemanticVersion, SemanticVersion> predicate) {
+            this.predicate = predicate;
         }
-        DEV = dev;
+    
+        public boolean test(SemanticVersion suppliedVersion, SemanticVersion expectedVersion) {
+            return predicate.test(suppliedVersion, expectedVersion);
+        }
+    }
+    
+    private enum CompatibilityLayer {
+        LOOM_FIX(null, null, VersionCheckType.ALWAYS, (mixinName)->
+            switch(mixinName){
+                case "net.gudenau.minecraft.moretags.mixins.farmland.RabbitEntity$EatCarrotCropGoalMixin" -> !DEV;
+                case "net.gudenau.minecraft.moretags.mixins.farmland.RabbitEntity$EatCarrotCropGoal$DevMixin" -> DEV;
+                default -> true;
+            }
+        ),
+        FAPI_TAGS("fabric-tag-extensions-v0", "1.2.2", VersionCheckType.OLDER_THAN, (mixinName)->
+            !"net.gudenau.minecraft.moretags.mixins.retro.OldTagApiSupport".equals(mixinName)
+        ),
+        // All the Blocks You Want bulldozes some of our injection points, at least they do the same thing we do...
+        ATBYW("atbyw", null, VersionCheckType.ALWAYS, (mixinName)->
+            switch(mixinName){
+                case "net.gudenau.minecraft.moretags.mixins.bookshelves.EnchantingTableBlockMixin",
+                    "net.gudenau.minecraft.moretags.mixins.bookshelves.EnchantmentScreenHandlerMixin" -> false;
+                default -> true;
+            }
+        ),
+        ;
+    
+        private final String modId;
+        private final String modVersion;
+        private final VersionCheckType checkType;
+        private final Predicate<String> mixinPredicate;
+    
+        CompatibilityLayer(String modId, String modVersion, VersionCheckType checkType, Predicate<String> mixinPredicate){
+            this.modId = modId;
+            this.modVersion = modVersion;
+            this.checkType = checkType;
+            this.mixinPredicate = mixinPredicate;
+        }
+    
+        public boolean shouldApply() {
+            // For the universal one.
+            if(modId == null){
+                return true;
+            }
+            
+            var container = FabricLoader.getInstance().getModContainer(modId);
+            if(container.isEmpty()){
+                return false;
+            }
+            // Allow null versions and short circuit.
+            if(checkType == VersionCheckType.ALWAYS){
+                return true;
+            }
+            var modVersion = container.get().getMetadata().getVersion();
+            if(!(modVersion instanceof SemanticVersion suppliedVersion)){
+                LOGGER.error("Failed to get SemanticVersion for mod %s".formatted(modId));
+                return false;
+            }
+            SemanticVersion expectedVersion = null;
+            try {
+                expectedVersion = SemanticVersion.parse(this.modVersion);
+            } catch (VersionParsingException e) {
+                throw new RuntimeException("Failed to parse version %s for %s's compatibility layer".formatted(this.modVersion, modId));
+            }
+            return checkType.test(suppliedVersion, expectedVersion);
+        }
+        
+        public boolean shouldApplyMixin(String mixinName){
+            return mixinPredicate.test(mixinName);
+        }
+    }
+    
+    private static final Set<CompatibilityLayer> COMPATIBILITY_LAYERS = Stream.of(CompatibilityLayer.values())
+        .filter(CompatibilityLayer::shouldApply)
+        .collect(Collectors.toUnmodifiableSet());
+    
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName){
+        for (var layer : COMPATIBILITY_LAYERS) {
+            if(!layer.shouldApplyMixin(mixinClassName)){
+                return false;
+            }
+        }
+        return true;
     }
     
     @Override
@@ -29,35 +125,6 @@ public final class Plugin implements IMixinConfigPlugin{
     @Override
     public String getRefMapperConfig(){
         return null;
-    }
-    
-    private boolean isModOlderThan(String id, String version){
-        var container = FabricLoader.getInstance().getModContainer(id);
-        if(container.isEmpty()){
-            return false;
-        }
-        var modVersion = container.get().getMetadata().getVersion();
-        if(!(modVersion instanceof SemanticVersion semanticVersion)){
-            throw new IllegalStateException("Weird fabric-tag-extensions-v0 version");
-        }
-        try{
-            return semanticVersion.compareTo(SemanticVersion.parse(version)) < 0;
-        }catch(VersionParsingException e){
-            throw new RuntimeException("Failed to parse version: " + version, e);
-        }
-    }
-    
-    @Override
-    public boolean shouldApplyMixin(String targetClassName, String mixinClassName){
-        if(mixinClassName.equals("net.gudenau.minecraft.moretags.mixins.retro.OldTagApiSupport")){
-            return isModOlderThan("fabric-tag-extensions-v0", "1.2.2");
-        }
-        
-        return switch(mixinClassName){
-            case "net.gudenau.minecraft.moretags.mixins.farmland.RabbitEntity$EatCarrotCropGoalMixin" -> !DEV;
-            case "net.gudenau.minecraft.moretags.mixins.farmland.RabbitEntity$EatCarrotCropGoal$DevMixin" -> DEV;
-            default -> true;
-        };
     }
     
     @Override
