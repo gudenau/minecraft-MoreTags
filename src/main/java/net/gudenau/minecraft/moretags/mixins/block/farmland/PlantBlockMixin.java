c9d20f364562309817347a1dd06da112ab3f0c9b
Jack Papel <jackpapel@live.com>
2022-03-13 21:29:08 -0700

    Move this to the correct directory

--- src/main/java/net/gudenau/minecraft/moretags/mixins/block/farmland/PlantBlockMixin.java
+++ src/main/java/net/gudenau/minecraft/moretags/mixins/block/farmland/PlantBlockMixin.java
@@ -1,11 +1,8 @@
 package net.gudenau.minecraft.moretags.mixins.block.farmland;
 
 import net.gudenau.minecraft.moretags.MoreBlockTags;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.PlantBlock;
-import net.minecraft.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,17 +10,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PlantBlock.class)
+@Mixin(value = PlantBlock.class, priority = 1001)
 public abstract class PlantBlockMixin {
     @Inject(
         method = "canPlantOnTop",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"
-        ),
+        at = @At("RETURN"),
         cancellable = true
     )
     private void canPlantOnTop(BlockState floor, BlockView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(floor.isIn(MoreBlockTags.PLANTABLE_BLOCKS));
+        cir.setReturnValue(cir.getReturnValue() || floor.isIn(MoreBlockTags.FARMLAND));
     }
 }
